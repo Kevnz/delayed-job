2faee540d61f12e8a1daf1602c8887f191a9c476
Kevin Isom <kevin@isom.nz>
2019-04-26 09:10:55 +1200

    Change up to use memory

--- examples/test.js
+++ examples/test.js
@@ -2,13 +2,16 @@ const Scheduler = require('../index.js')
 
 const scheduler = Scheduler.createScheduler({
   backend: {
-    name: 'redis',
+    name: 'memory',
     jobHoldingBay: 'myTestHoldingKey',
   },
 })
+const now = Date.now()
 
 scheduler.on('job', job => {
   console.log('Received job', job)
+  const end = Date.now()
+  console.log('duration', end - now)
   // exit
   process.exit(0)
 })
@@ -17,9 +20,10 @@ const myJob = {
   title: 'Great Gig In The Sky',
 }
 
-scheduler.delay(myJob, 1000)
+scheduler.delay(myJob, 10)
 
 setTimeout(() => {
   // if this fires something is broken
+  console.error('This should not be called')
   process.exit(1)
 }, 8000)
